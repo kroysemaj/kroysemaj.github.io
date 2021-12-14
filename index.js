ccdec21f8de194192fed57f4f0eab12f6ebf7b6d
James York <kroysemaj@gmail.com>
2021-12-14 03:54:37 -0500

    Add some randomized peek a boo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const easterEggs = ["luigi", "mario", "spongebob", "squidward1", "squidward2"];
+
+function randomizeEl() {
+  return easterEggs[Math.floor(Math.random() * easterEggs.length)];
+}
+
 $(document).ready(() => {
-  $(window).load(function () {
-    $(".raptor").raptorize({
-      enterOn: "click",
-    });
-  });
+  $("#secret").click(() => $(`#${randomizeEl()}`).fadeToggle(250));
 });
